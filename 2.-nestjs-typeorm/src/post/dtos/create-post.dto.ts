25875f80e772aa6e37268e40f612686c8eaeef04
jonmolinna <jmolina2624@gmail.com>
2021-11-16 09:09:24 -0500

    dtos

---
+++ 2.-nestjs-typeorm/src/post/dtos/create-post.dto.ts
@@ -0,0 +1,31 @@
+import { PostCategory } from '../enums';
+import { IsArray, IsBoolean, IsEnum, IsString } from 'class-validator';
+import { EnumToString } from 'src/helpers/enumToString';
+
+export class CreatePostDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  slug: string;
+
+  @IsString()
+  excerpt: string;
+
+  @IsString()
+  content: string;
+
+  @IsEnum(PostCategory, {
+    message: `Opcion invalida. Las opciones correctas son ${EnumToString(
+      PostCategory,
+    )}`,
+  })
+  category: PostCategory;
+
+  @IsArray()
+  @IsString({ each: true })
+  tags: string[];
+
+  @IsBoolean()
+  status: boolean;
+}
