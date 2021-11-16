909519254c3191b6198643bb7211a479a845e600
jonmolinna <jmolina2624@gmail.com>
2021-11-16 09:10:54 -0500

    controller

---
+++ 2.-nestjs-typeorm/src/post/post.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreatePostDto, EditPostDto } from './dtos';
+import { PostService } from './post.service';
+
+@Controller('post')
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Get()
+  async getMany() {
+    const data = await this.postService.getMany();
+    return {
+      message: 'Peticion correcta',
+      data,
+    };
+  }
+
+  @Get('/:id')
+  getOne(@Param('id', ParseIntPipe) id: number) {
+    return this.postService.getOne(id);
+  }
+
+  @Post()
+  createOne(@Body() dto: CreatePostDto) {
+    return this.postService.createOne(dto);
+  }
+
+  @Put('/:id')
+  editOne(@Param('id') id: number, @Body() dto: EditPostDto) {
+    return this.postService.editOne(id, dto);
+  }
+
+  @Delete('/:id')
+  deleteOne(@Param('id') id: number) {
+    return this.postService.deleteOne(id);
+  }
+}
