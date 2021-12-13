835e99f203c6a934b85eb667b7e518480ef3f21b
jonmolinna <jmolina2624@gmail.com>
2021-12-13 10:34:10 -0500

    Sistema de autenticación

---
+++ 4.-nestjs-auth/src/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { UsersRepository } from './users.repository';
+import { JwtModule } from '@nestjs/jwt';
+
+import { EncoderService } from './encoder.service';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'super-secret',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([UsersRepository]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, EncoderService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
+})
+export class AuthModule {}
---
+++ 4.-nestjs-auth/src/auth/users.repository.ts
@@ -0,0 +1,60 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+
+import { User } from './user.entity';
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+  async createUser(
+    name: string,
+    email: string,
+    password: string,
+    activationToken: string,
+  ): Promise<void> {
+    const user = this.create({ name, email, password, activationToken });
+
+    try {
+      await this.save(user);
+    } catch (e) {
+      if (e.code === 'ER_DUP_ENTRY') {
+        throw new ConflictException('This email is already registered');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async findOneByEmail(email: string): Promise<User> {
+    const user: User = await this.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundException(`User with email ${email} not found`);
+    }
+
+    return user;
+  }
+
+  async activateUser(user: User): Promise<void> {
+    user.active = true;
+    this.save(user);
+  }
+
+  async findOneInactiveByIdAndActivationToken(
+    id: string,
+    code: string,
+  ): Promise<User> {
+    return this.findOne({ id: id, activationToken: code, active: false });
+  }
+
+  async findOneByResetPasswordToken(resetPasswordToken: string): Promise<User> {
+    const user: User = await this.findOne({ resetPasswordToken });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+}
